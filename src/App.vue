88d5049480c24463ff9bf72d43d7c1ac2f6f51ca
unknown <1368106265@qq.com>
2020-12-31 16:12:19 +0800

    '初始化'

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="app">
-    <tab-bar></tab-bar>
+    <tab-bar>
+      <tab-bar-item></tab-bar-item>
+      <tab-bar-item></tab-bar-item>
+      <tab-bar-item></tab-bar-item>
+      <tab-bar-item></tab-bar-item>
+    </tab-bar>
   </div>
 </template>
 
 <script>
 import TabBar from './components/common/TabBar/TabBar'
+import TabBarItem from './components/common/TabBar/TabBarItem'
 
 export default {
   name: 'App',
   components: {
-    TabBar
+    TabBar,
+    TabBarItem
   }
 }
 </script>
--- src/components/common/TabBar/TabBar.vue
+++ src/components/common/TabBar/TabBar.vue
@@ -1,33 +1,20 @@
 <template>
   <div id="tab-bar">
-    <div class="tab-bar-item">
-      <img src="assets/img/tabbar/home.svg" alt="" />
-      首页
-    </div>
-    <div class="tab-bar-item">
-      <img src="assets/img/tabbar/home.svg" alt="" />
-      分类
-    </div>
-    <div class="tab-bar-item">
-      <img src="assets/img/tabbar/home.svg" alt="" />
-      购物车
-    </div>
-    <div class="tab-bar-item">
-      <img src="assets/img/tabbar/home.svg" alt="" />
-      我的
-    </div>
+    <slot></slot>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name:'TabBar'
+};
 </script>
 
-<style scoped>
+<style>
 #tab-bar {
   display: flex;
   height: 49px;
-  line-height: 49px;
+  /* line-height: 49px; */
   background-color: #f6f6f6;
   position: fixed;
   left: 0;
@@ -35,8 +22,4 @@ export default {};
   bottom: 0;
   box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.3);
 }
-.tab-bar-item {
-  flex: 1;
-  text-align: center;
-}</style
->>
+</style>

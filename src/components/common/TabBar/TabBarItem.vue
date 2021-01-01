006851f78f8a8fe1a5575df5c486bcbc70f56df1
unknown <1368106265@qq.com>
2021-01-01 11:21:09 +0800

    1

--- src/components/common/TabBar/TabBarItem.vue
+++ src/components/common/TabBar/TabBarItem.vue
@@ -1,14 +1,31 @@
 <template>
-  <div class="tab-bar-item">
-      <slot name="item-icon-acitve" ></slot>
-      <slot name="item-icon" ></slot>
+  <div class="tab-bar-item" @click="itemClick">
+      <div v-if="!isActive">
+        <slot name="item-icon"></slot>
+      </div>
+      <div v-else> 
+        <slot name="item-icon-active"></slot>
+      </div> 
       <slot name="item-text"></slot>
   </div>
 </template>
 
 <script>
 export default {
-  name:'TabBarItem'
+  name:'TabBarItem',
+  props:{
+    path:String
+  },
+  computed:{
+    isActive() {
+      this.$route.path.indexOf(this.path) !== -1
+    }
+  },
+  methods:{
+    itemClick() {
+      this.$router.push(this.path)
+    }
+  }
 };
 </script>
 
@@ -20,8 +37,6 @@ export default {
 }
 .tab-bar-item img {
   width: 24px;
-  height:24px;
-  margin-top: 3px;
-  vertical-align: middle;
+  height: 24px;
 }
 </style>

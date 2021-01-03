28bd7d6e2f2f0ba479809f4b3b1b7a00b22e337b
2018qdly <lymxhh@163.com>
2021-01-03 12:54:26 +0800

    '1'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="home">
-
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childcomps/HomeSwiper'
 export default {
   components: {
-
+    NavBar,
+    HomeSwiper
   },
 
 
@@ -15,5 +19,8 @@ export default {
 </script>
 
 <style>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
---
+++ src/views/home/childcomps/HomeSwiper.vue
@@ -0,0 +1,34 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners" :key="item.title">
+      <a :href="item.link">
+        <img :src="item.image" :alt="item.title">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from "components/common/swiper";
+import {getHomeMultidata} from "network/home";
+import {getHomeGoods} from "network/home";
+export default {
+  data() {
+    return {
+      banners: [],
+    };
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      this.banners = res.data.banner.list;
+    });
+  },
+  components:{
+    Swiper,
+    SwiperItem
+  }
+};
+</script>
+
+<style>
+</style>

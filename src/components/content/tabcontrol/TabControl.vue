c9c3f16eb927d362cc558c2c7d9e61899b08fa58
2018qdly <lymxhh@163.com>
2021-01-03 18:06:44 +0800

    '1'

---
+++ src/components/content/tabcontrol/TabControl.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tab-control">
+    <div
+    v-for="(item,index) in titles"
+    class="tab-control-item"
+    :class="{active:curindex === index}"
+    @click="itemclick(index)"
+    >
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      curindex:0
+    }
+  },
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  methods:{
+    itemclick(index) {
+      this.curindex = index
+    }
+  }
+};
+</script>
+
+<style>
+.tab-control {
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  height: 40px;
+  line-height: 40px;
+}
+.tab-control-item {
+  flex: 1;
+}
+.active {
+  color:deeppink;
+}
+.active span {
+  border-bottom:deeppink 2px solid;
+  padding: 3px;
+}
+</style>
